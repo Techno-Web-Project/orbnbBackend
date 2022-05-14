fb622a3aea755d4714432bd87cbd594d189d97ea
MorganDelange <74045908+MorganDelange@users.noreply.github.com>
2022-05-14 18:27:38 +0200

    delete, add and replace method added for Person

--- src/main/java/orbnb/backend/controller/PersonController.java
+++ src/main/java/orbnb/backend/controller/PersonController.java
@@ -3,10 +3,7 @@ package orbnb.backend.controller;
 import orbnb.backend.model.Person;
 import orbnb.backend.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,5 +23,36 @@ public class PersonController {
     @GetMapping("getPerson/{id}")
     public Optional<Person> getPersonById(@PathVariable long id){
         return this.personRepository.findById(id);
+    }
+
+    @PostMapping("newPerson")
+    Person addPerson(@RequestBody Person newPerson) {
+        return personRepository.save(newPerson);
+    }
+
+    @PutMapping("replacePerson/{id}")
+    Person replacePerson(@RequestBody Person newPerson, @PathVariable Long id) {
+
+        return personRepository.findById(id)
+                .map(person -> {
+                    person.setFirstName(newPerson.getFirstName());
+                    person.setLastName(newPerson.getLastName());
+                    person.setPhoneNumber(newPerson.getPhoneNumber());
+                    person.setEmail(newPerson.getEmail());
+                    person.setLogin(newPerson.getLogin());
+                    person.setPassword(newPerson.getPassword());
+                    person.setPicture(newPerson.getPicture());
+                    person.setBirthDate(newPerson.getBirthDate());
+                    return personRepository.save(person);
+                })
+                .orElseGet(() -> {
+                    newPerson.setId(id);
+                    return personRepository.save(newPerson);
+                });
+    }
+
+    @GetMapping("deletePerson/{id}")
+    void deleteEmployee(@PathVariable Long id) {
+        personRepository.deleteById(id);
     }
 }
