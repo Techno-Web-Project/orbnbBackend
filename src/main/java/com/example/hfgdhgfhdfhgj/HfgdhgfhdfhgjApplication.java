810515324fab6d469db410455b8f625853dd7116
MorganDelange <74045908+MorganDelange@users.noreply.github.com>
2022-05-14 17:25:45 +0200

    initial commit

---
+++ src/main/java/com/example/hfgdhgfhdfhgj/HfgdhgfhdfhgjApplication.java
@@ -0,0 +1,13 @@
+package com.example.hfgdhgfhdfhgj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HfgdhgfhdfhgjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HfgdhgfhdfhgjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hfgdhgfhdfhgj/HfgdhgfhdfhgjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hfgdhgfhdfhgj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HfgdhgfhdfhgjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
